11daaac96a6b9dd64b3e00f1390cf5810df83209
spartak.gareginyan <gareginyans@gmail.com>
2022-09-28 18:13:27 +0400

    AOP static pointcut.

---
+++ src/main/java/com/apress/prospring5/ch5/GoodGuitarist.java
@@ -0,0 +1,9 @@
+package com.apress.prospring5.ch5;
+
+public class GoodGuitarist implements Singer {
+    @Override
+    public void sing() {
+        System.out.println("Who says I can't be free \n"
+                               + "From all of the things that I used to be");
+    }
+}

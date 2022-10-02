d8e65dbfe6ea8cc5c38e66573f824408e0f14553
Spartak <gareginyans@mail.ru>
2022-10-02 21:44:45 +0400

    AOP name pointcut.

---
+++ src/main/java/com/apress/prospring5/ch5/GrammyGuitarist.java
@@ -0,0 +1,21 @@
+package com.apress.prospring5.ch5;
+
+public class GrammyGuitarist implements Singer {
+    @Override
+    public void sing() {
+        System.out.println("sing: Gravity is working against me\n" +
+                "And gravity wants to bring me down");
+    }
+
+    public void sing(Guitar guitar) {
+        System.out.println("play: " + guitar.play());
+    }
+
+    public void rest() {
+        System.out.println("zzz");
+    }
+
+    public void talk() {
+        System.out.println("talk");
+    }
+}
---
+++ src/main/java/com/apress/prospring5/ch5/Guitar.java
@@ -0,0 +1,7 @@
+package com.apress.prospring5.ch5;
+
+public class Guitar {
+    public String play() {
+        return "Cm Eb Fm Ab Bb";
+    }
+}

aa8627385c520a768939fefd48956187502075d4
spartak.gareginyan <gareginyans@mail.ru>
2022-10-20 18:40:15 +0400

    AOP aspectj style.

---
+++ src/main/java/com/apress/prospring5/ch5/aspectj/AppConfig.java
@@ -0,0 +1,11 @@
+package com.apress.prospring5.ch5.aspectj;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = {"com.apress.prospring5.ch5.aspectj"})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class AppConfig {
+}
